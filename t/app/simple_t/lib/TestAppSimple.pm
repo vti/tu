1c679af51973a9f6dd25db51037508d2d480b3a3
vti <viacheslav.t@gmail.com>
2014-12-18 22:31:32 +0200

    fix plugin names

--- t/app/simple_t/lib/TestAppSimple.pm
+++ t/app/simple_t/lib/TestAppSimple.pm
@@ -8,7 +8,8 @@ use base 'Tu';
 sub startup {
     my $self = shift;
 
-    $self->register_plugin('DefaultServices');
+    $self->register_plugin('CommonServices');
+    $self->register_plugin('CommonMiddleware');
 
     $self->service('routes')->add_route('/', name => 'index');
 }
