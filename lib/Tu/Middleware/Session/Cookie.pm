1c68c54dc0b8260d1518cf5c68582377f24b8d92
vti <viacheslav.t@gmail.com>
2015-01-11 14:24:01 +0200

    revert

--- lib/Tu/Middleware/Session/Cookie.pm
+++ lib/Tu/Middleware/Session/Cookie.pm
@@ -17,38 +17,4 @@ sub new {
     return $class->SUPER::new(%$config, %params);
 }
 
-sub call {
-    my $self = shift;
-    my $env  = shift;
-
-    my ($id, $session) = $self->get_session($env);
-    if ($id && $session) {
-        $env->{'psgix.session'} = $session;
-    }
-    else {
-        $id = $self->generate_id($env);
-        $env->{'psgix.session'} = {};
-    }
-
-    $env->{'psgix.session.options'} = {id => $id};
-
-    my $res;
-    eval { $res = $self->app->($env) } or do {
-        my $e = $@;
-
-        $self->_finalize($res, $env) if $res;
-
-        die $@;
-    };
-
-    return $self->_finalize($res, $env);
-}
-
-sub _finalize {
-    my $self = shift;
-    my ($res, $env) = @_;
-
-    return $self->response_cb($res, sub { $self->finalize($env, $_[0]) });
-}
-
 1;
